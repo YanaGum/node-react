b20d191c085c9d6d3b986d85e5e3d996eddfce0d
unknown <gumenukyana19012001@gmail.com>
2021-03-14 10:27:47 +0200

    add eslint,prettier

--- server/index.js
+++ server/index.js
@@ -1,26 +1,28 @@
 require('dotenv').config();
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || "3000";
+const PORT = process.env.PORT || '3000';
 
 if (process.env.NODE_ENV !== 'production') {
-    app.use(
-      cors({
-        origin: '*',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-      }),
-    );
-  }
+  app.use(
+    cors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }),
+  );
+}
 
-app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
-  });
+app.get('/', (req, res) => {
+  res.status(200).send('WHATABYTE: Food For Devs');
+});
 
 app.listen(PORT, () => {
-    console.log(`Server has been started on port http://localhost:${process.env.PORT}...`);
-  });
+  console.log(
+    `Server has been started on port http://localhost:${process.env.PORT}...`,
+  );
+});
